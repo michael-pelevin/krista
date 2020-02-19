05e04b56ca10e2da39ee30551c116e76554bfb4d
Ratnikov Egor <hiragen@yandex.ru>
2020-02-19 16:10:44 +0200

    first task

---
+++ FirstProjectKrista/src/main/java/ConsoleApp.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import static java.lang.System.out;
+
+public class ConsoleApp {
+
+    public static void main(String[] argv) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
+    }
+
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        out.printf("Display name: %s\n", netint.getDisplayName());
+        out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+
+        byte[] mac = netint.getHardwareAddress();
+        System.out.print("Current MAC address : ");
+
+        StringBuilder sb = new StringBuilder();
+        if (mac != null) {
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            System.out.println(sb.toString());
+        }
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+        out.printf("\n");
+
+    }
+}
